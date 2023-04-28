0c07fe2b7b678577e23a39abf897efc568d4a538
sreelakshmi <131979219+sreeluk@users.noreply.github.com>
2023-04-28 20:42:03 +0530

    commit changes in git hub
    ...........

--- printusrpass.sh
+++ printusrpass.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #script for printing username and passwd
-read -p " enter the user name:" name
-read -sp "enter the passwword:" pass
+read -p " enter the user name with you:" name
+read -sp "enter the passwword of user:" pass
 useradd $name
 echo "$name:$pass"|chpasswd
 echo -e "\n user $name created...."
